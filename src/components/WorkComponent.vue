e514234a1903edd9965d11b5059312f4334e2ee8
steelswheel <steelswheel@gmail.com>
2023-04-09 18:31:31 +0400

    fix

--- src/components/WorkComponent.vue
+++ src/components/WorkComponent.vue
@@ -180,26 +180,6 @@
                     }
                 ]
             }
-        },
-        methods: {
-            onResize() {
-                let width = document.documentElement.clientWidth;
-
-                this.align = width > 575 ? 'alternate' : '';
-            },
-            dispatchResizeEvent() {
-                //используется для обхода бага библиотеки AOS
-                window.dispatchEvent(new Event('resize'));
-                console.log('работает');
-            }
-        },
-        mounted() {
-            window.addEventListener('resize', this.onResize);
-            window.addEventListener('load', this.dispatchResizeEvent);
-        },
-        unmount() {
-            window.removeEventListener('resize', this.onResize);
-            window.removeEventListener('load', this.dispatchResizeEvent);
         }
     }
 </script>
